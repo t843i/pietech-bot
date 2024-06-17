60450c6feb9dee3f89e23cee777bbe31dc9f01f8
pieiskewl <cewtoo517@gmail.com>
2024-06-17 15:54:31 -0700

    fix: added path & fs

--- src/events/ready/webAppStart.js
+++ src/events/ready/webAppStart.js
@@ -1,6 +1,8 @@
 require('colors');
 
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const {
   getRobloxPicture,
