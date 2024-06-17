1adae26bdda76f9aaa5df32efc4fe4fff2edf501
pieiskewl <cewtoo517@gmail.com>
2024-06-17 15:27:29 -0700

    fix: added gitignore

--- src/utils/deploy.js
+++ src/utils/deploy.js
@@ -1,7 +1,7 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-require('dotenv').config({path:__dirname+'../.env'});
+require('dotenv').config();
 
 let token = process.env.TOKEN;
 let guildId = process.env.GUILD_ID;
