c9222c0f739898950a4d01800ef2403cd8b59c77
pieiskewl <cewtoo517@gmail.com>
2024-06-17 15:18:05 -0700

    fix: env work

--- src/utils/deploy.js
+++ src/utils/deploy.js
@@ -1,7 +1,7 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-require('dotenv').config();
+require('dotenv').config({path:__dirname+'../.env'});
 
 let token = process.env.TOKEN;
 let guildId = process.env.GUILD_ID;
