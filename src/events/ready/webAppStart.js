9283343bc98f6e91131c7d3ebb0f9758f9297ccb
pieiskewl <cewtoo517@gmail.com>
2024-06-29 12:33:20 -0700

    feat: more stuff!

--- src/events/ready/webAppStart.js
+++ src/events/ready/webAppStart.js
@@ -172,7 +172,7 @@ module.exports = async (client) => {
         '666e05969c7355c52e253cb8'
       );
 
-      res.redirect('https://discord.gg/cFXhwAhwhJ');
+      res.redirect('https://discord.gg/WuNJCBMyGB');
     });
 
     app.use('/api', sendInfoRouter);
